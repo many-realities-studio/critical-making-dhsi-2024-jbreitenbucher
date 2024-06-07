665abece78106d4edad397d767d6b4b8c204d236
Jon Breitenbucher <jon@breitenbucher.net>
2024-06-07 15:49:52 +0000

    Jun 7, 2024, 11:49 AM

--- sketchbook/4/script.js
+++ sketchbook/4/script.js
@@ -1,37 +1,39 @@
-function setup() {
-  createCanvas(400, 400);
-    }
+(function(){
+  function setup() {
+    createCanvas(400, 400);
+  }
 
-function draw() {
-  background(30);
-  drawCandle();
-}
+  function draw() {
+    background(30);
+    drawCandle();
+  }
 
-function drawCandle() {
-  // Candle properties
-  let candleX = width / 2;
-  let candleY = height * 3 / 4;
-  let candleWidth = 40;
-  let candleHeight = 100;
+  function drawCandle() {
+    // Candle properties
+    let candleX = width / 2;
+    let candleY = height * 3 / 4;
+    let candleWidth = 40;
+    let candleHeight = 100;
 
-  // Draw candle body
-  noStroke();
-  fill(200, 150, 100);
-  rect(candleX - candleWidth / 2, candleY - candleHeight, candleWidth, candleHeight);
+    // Draw candle body
+    noStroke();
+    fill(200, 150, 100);
+    rect(candleX - candleWidth / 2, candleY - candleHeight, candleWidth, candleHeight);
 
-  // Draw wick
-  fill(0);
-  rect(candleX - 2, candleY - candleHeight - 10, 4, 10);
+    // Draw wick
+    fill(0);
+    rect(candleX - 2, candleY - candleHeight - 10, 4, 10);
 
-  // Draw flame
-  let flameX = candleX + random(-2, 2);
-  let flameY = candleY - candleHeight - 20 + random(-2, 2);
-  let flameSize = 30 + random(-5, 5);
+    // Draw flame
+    let flameX = candleX + random(-2, 2);
+    let flameY = candleY - candleHeight - 20 + random(-2, 2);
+    let flameSize = 30 + random(-5, 5);
 
-  noStroke();
-  fill(255, 140, 0, 150);
-  ellipse(flameX, flameY, flameSize, flameSize * 2);
+    noStroke();
+    fill(255, 140, 0, 150);
+    ellipse(flameX, flameY, flameSize, flameSize * 2);
 
-  fill(255, 200, 0, 200);
-  ellipse(flameX, flameY, flameSize / 2, flameSize);
-}
+    fill(255, 200, 0, 200);
+    ellipse(flameX, flameY, flameSize / 2, flameSize);
+  }
+})
