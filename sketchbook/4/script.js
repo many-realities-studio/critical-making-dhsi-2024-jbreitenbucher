687810f4341dd38abfb4dfdbee0d37b6c4186e9e
Jon Breitenbucher <jon@breitenbucher.net>
2024-06-07 11:46:15 -0400

    change

--- sketchbook/4/script.js
+++ sketchbook/4/script.js
@@ -1,9 +1,37 @@
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  print("Hello world")
-}
+  createCanvas(400, 400);
+    }
 
 function draw() {
-  // Draw a circle in the center of the screen
-  circle(width / 2, height / 2, 50)
+  background(30);
+  drawCandle();
+}
+
+function drawCandle() {
+  // Candle properties
+  let candleX = width / 2;
+  let candleY = height * 3 / 4;
+  let candleWidth = 40;
+  let candleHeight = 100;
+
+  // Draw candle body
+  noStroke();
+  fill(200, 150, 100);
+  rect(candleX - candleWidth / 2, candleY - candleHeight, candleWidth, candleHeight);
+
+  // Draw wick
+  fill(0);
+  rect(candleX - 2, candleY - candleHeight - 10, 4, 10);
+
+  // Draw flame
+  let flameX = candleX + random(-2, 2);
+  let flameY = candleY - candleHeight - 20 + random(-2, 2);
+  let flameSize = 30 + random(-5, 5);
+
+  noStroke();
+  fill(255, 140, 0, 150);
+  ellipse(flameX, flameY, flameSize, flameSize * 2);
+
+  fill(255, 200, 0, 200);
+  ellipse(flameX, flameY, flameSize / 2, flameSize);
 }
